03c8e7ab822e3fd84d0eb5b3bd79e448858b3972
sonic25 <sofiagoncearp2313@gmail.com>
2026-02-02 19:01:52 +0200

    2nd version

--- src/main/java/com/impact/lessons/constants/AppConstant.java
+++ src/main/java/com/impact/lessons/constants/AppConstant.java
@@ -3,7 +3,7 @@ package com.impact.lessons.constants;
 public class AppConstant {
     public AppConstant(){}
 
-    public static final String HelloWorld ="Hello World ";
+    public static final String HelloWorld ="Hello ";
 }
 
 
